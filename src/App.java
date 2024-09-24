daafb1f8c709128b69af323856ee4a61a99619fd
Noah K <27nklebanov@gannacademy.org>
2024-09-24 09:14:30 -0400

    done

--- src/App.java
+++ src/App.java
@@ -4,4 +4,26 @@ public class App extends PApplet {
     public static void main(String[] args)  {
         PApplet.main("App");
     }
+        public void settings(){
+            size(400,400);
+        }
+        public void setup(){
+            background(200);
+
+            fill(64,224,208);
+            ellipse(200,120,100,100);
+
+            fill(255,127,80);
+            triangle(150,250,200,337,250,250);
+
+            stroke(0);
+            strokeWeight(3);
+            line(200,120,200,294);
+
+        }
+        public void draw(){
+
+        }
+    
+
 }
